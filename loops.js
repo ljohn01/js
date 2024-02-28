1754d6236eab2c0205f432c7e298555ad0f101a8
ljohn01 <159062024+ljohn01@users.noreply.github.com>
2024-02-28 13:23:41 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,37 @@
+// const numberID = ["42", "57", "63", "98"];
+// const firstName = ["Bruce", "Bob", "Blaine", "Bill"];
+// const jobRole = ["Knight", "Fry Cook", "Quiz Master", "Doctor's Assistant"];
+// const realAge = ["41", "19", "58", "26"];
+
+// let cell1 = numberID.slice(0,4);
+// let cell2 = firstName.slice(0,4);
+// let cell3 = jobRole.slice(0,4);
+// let cell4 = realAge.slice(0,4);
+
+
+let cell1 = "ID";
+let cell2 = "Name";
+let cell3 = "Occupation";
+let cell4 = "Age";
+
+let cell5 = "42";
+let cell6 = "Bruce";
+let cell7 = "Knight";
+let cell8 = "41";
+
+let cell9 = "57";
+let cell10 = "Bob";
+let cell11 = "Fry Cook";
+let cell12 = "19";
+
+let cell13 = "63";
+let cell14 = "Blaine";
+let cell15 = "Quiz Master";
+let cell16 = "58";
+
+let cell17 = "98";
+let cell18 = "Bill";
+let cell19 = "Doctor's Assistant";
+let cell20 = "26";
+
+console.log(cell1, cell2, cell3, cell4);
