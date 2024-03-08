76e56e9d627044b675f9055f43d0bc373d582a33
ljohn01 <159062024+ljohn01@users.noreply.github.com>
2024-03-08 11:35:36 -0500

    Update object.js

--- 2024-03-08-GLAB-object-adventure/object.js
+++ 2024-03-08-GLAB-object-adventure/object.js
@@ -26,25 +26,25 @@ const adventurer = {
 adventurer.roll();
 
 // Part 2: Class Fantasy
-class Character {
-    constructor (name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
-    }
-    roll(mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-    }
-  }
+// class Character {
+//     constructor (name) {
+//       this.name = name;
+//       this.health = 100;
+//       this.inventory = [];
+//     }
+//     roll(mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//     }
+//   }
 
-  const robin = new Character("Robin");
-  robin.inventory = ["sword", "potion", "artifact"];
-  robin.companion = new Character("Leo");
-  robin.companion.type = "Cat";
-  robin.companion.companion = new Character("Frank");
-  robin.companion.companion.type = "Flea";
-  robin.companion.companion.inventory = ["small hat", "sunglasses"];
+//   const robin = new Character("Robin");
+//   robin.inventory = ["sword", "potion", "artifact"];
+//   robin.companion = new Character("Leo");
+//   robin.companion.type = "Cat";
+//   robin.companion.companion = new Character("Frank");
+//   robin.companion.companion.type = "Flea";
+//   robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 // Part 3: Class Features
 // Previous code from part 1
@@ -120,12 +120,61 @@ class Character {
 // }
 
 // Part 4: Class Uniforms (will start going over in class Monday)
-
-
+class Character {
+    static MAX_HEALTH = 100;
+  
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  }
+  
+  class Adventurer extends Character {
+    static ROLES = ["Fighter", "Healer", "Wizard", "Archer", "Mage"];
+  
+    constructor(name, role) {
+      console.log(Adventurer.ROLES);
+      super(name);
+      if (Adventurer.ROLES.includes(role)) {
+        this.role = role; 
+      } else {
+        
+        let length = Adventurer.ROLES.length;
+        let randomNum = Math.floor(Math.random(length));
+        this.role = Adventurer.ROLES[randomNum];
+      }
+      // Every adventurer starts with a healing potion and 100 gold coins
+      this.inventory.push("healpot", "100 gold coins");
+    }
+  }
 
 
 // Part 5: Gather Your Party
-
+class AdventurerFactory {  
+    constructor (role) {
+      this.role = role;
+      this.adventurers = [];
+    }
+    generate (name) {
+      const newAdventurer = new Adventurer(name, this.role);
+      this.adventurers.push(newAdventurer);
+    }
+    findByIndex (index) {
+      return this.adventurers[index];
+    }
+    findByName (name) {
+      return this.adventurers.find((a) => a.name === name);
+    }
+  }
+  
+  const healers = new AdventurerFactory("Healer");
+  const robin = healers.generate("Robin");
 
 
 
